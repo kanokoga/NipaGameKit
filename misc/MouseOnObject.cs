e012407c205fdaf7aca9a136e68aeaac18d14c3c
kanokoga <kanokoga@gmail.com>
2025-12-30 20:20:37 +0900

    update:

---
+++ misc/MouseOnObject.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System;
+
+namespace NipaGameKit
+{
+    public enum MouseActionType
+    {
+        None,
+        MouseEnter,
+        MouseExit,
+        MouseClick
+    }
+
+    public class MouseOnObject : MonoBehaviour
+    {
+        public event Action<MouseActionType> OnMouseAction = delegate { };
+
+        private void OnMouseEnter()
+        {
+            this.OnMouseAction(MouseActionType.MouseEnter);
+        }
+
+        private void OnMouseExit()
+        {
+            this.OnMouseAction(MouseActionType.MouseExit);
+        }
+
+        private void OnMouseDown()
+        {
+            this.OnMouseAction(MouseActionType.MouseClick);
+        }
+
+        private void OnDestroy()
+        {
+            this.OnMouseAction = delegate { };
+        }
+    }
+}
