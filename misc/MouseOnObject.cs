8dd5e9198737badb58f343d266c2b01d24bc74eb
kanokoga <kanokoga@gmail.com>
2026-01-11 18:55:29 +0900

    updat:

--- misc/MouseOnObject.cs
+++ misc/MouseOnObject.cs
@@ -13,25 +13,46 @@ namespace NipaGameKit
 
     public class MouseOnObject : MonoBehaviour
     {
+        public bool IsActive { get; set; } = true;
         public event Action<MouseActionType> OnMouseAction = delegate { };
+
+
+        public void SetActive(bool active)
+            => this.IsActive = active;
 
         private void OnMouseEnter()
         {
+            if(this.IsActive == false)
+            {
+                return;
+            }
             this.OnMouseAction(MouseActionType.MouseEnter);
         }
 
         private void OnMouseExit()
         {
+            if(this.IsActive == false)
+            {
+                return;
+            }
             this.OnMouseAction(MouseActionType.MouseExit);
         }
 
         private void OnMouseUp()
         {
+            if(this.IsActive == false)
+            {
+                return;
+            }
             this.OnMouseAction(MouseActionType.MouseClick);
         }
 
         private void OnDestroy()
         {
+            if(this.IsActive == false)
+            {
+                return;
+            }
             this.OnMouseAction = delegate { };
         }
     }
