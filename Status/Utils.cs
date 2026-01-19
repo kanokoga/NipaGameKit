98bdbd22cb7b24a8557121669ae8f384e2420068
kanokoga <kanokoga@gmail.com>
2026-01-19 22:15:15 +0900

    update:

--- Status/Utils.cs
+++ Status/Utils.cs
@@ -5,10 +5,17 @@ namespace NipaGameKit.Statuses
 {
     public static class StatHelper
     {
-        public static string GetPercentage(float percentage)
+        public static string GetPercentageWithPrefix(float percentage)
         {
             var signPrefix = percentage >= 0 ? "+" : "-";
             return $"{signPrefix}{Mathf.Abs(percentage) * 100f:0.#}%";
         }
+
+        public static string GetPercentage(float percentage)
+        {
+            return $"{percentage * 100f:0.#}%";
+        }
+
+
     }
 }
